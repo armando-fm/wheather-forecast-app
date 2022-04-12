03d9242638ac673b5399a20c3b314d4fd5b9fc02
Armando Monjane <armando.monjane@portmaputo.com>
2022-04-12 09:27:47 +0200

    fix: Fix environment.prod.ts constants

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,9 @@
 export const environment = {
   production: true,
   // @ts-ignore
-  openWeatherPubKey: window['env']['openWeatherPubKey'] || ''
+  openWeatherPubKey: window['env']['openWeatherPubKey'] || '',
+  // @ts-ignore
+  openWeatherApiBaseUrl: window['env']['openWeatherApiBaseUrl'] || 'https://api.openweathermap.org/data/2.5',
+  // @ts-ignore
+  openWeatherGeocodingApiBaseUrl: window['env']['openWeatherGeocodingApiBaseUrl'] || 'http://api.openweathermap.org/geo/1.0',
 };
